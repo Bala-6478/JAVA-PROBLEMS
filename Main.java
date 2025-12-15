723d1f830b2a09d0c03621465f7dbf142e7fed26
BALAVIGNESH A <balavip073@gmail.com>
2025-12-15 11:11:58 +0530

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,9 @@
+public class Main{
+    public void pmassage(){
+        System.out.println("hello");
+    }
+    public static void main(String[] args) {
+        Main obj=new Main();
+        obj.pmassage();
+    }
+}
